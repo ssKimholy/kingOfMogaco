e70ab89c138e395a897af93c2ac8d7be18b86bbe
kingOfSchottland <46346267+kingOfSchottland@users.noreply.github.com>
2023-01-27 13:21:35 +0900

    Create App.js

---
+++ react/4주차/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/hello">
+        <Route path="/abot-us">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/movie">
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+	);
+}
